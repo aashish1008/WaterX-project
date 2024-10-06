af036e6fb371b91b676bbaa4158a9b8c7ff0fa0d
Aashish Shrestha <97799671+aashish1008@users.noreply.github.com>
2024-10-06 11:56:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from huggingface_hub import login
 
 class Quantized_Phi3:
     def __init__(self, model_name):
-        login(token="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")
+        login(token="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")
         self.model_name = model_name
         self.tokenizer = None
         self.model = None
